55cabae28efe007f8aa580eb307e275617a60e54
devzorsmallz <46795817+devzorsmallz@users.noreply.github.com>
2019-08-29 16:32:47 -0600

    parsing and manipulating strings

--- some_code.cs
+++ some_code.cs
@@ -1,1 +1,26 @@
-/* Insert Code */
+public class Example
+{
+    public static void Main()
+    {
+        string phrase = "Please don't take my bacon.";
+        /* Split the phrase into objects using ' ' to separate them, and put those objects into an array */
+        string[] words = phrase.Split(' ');
+
+        foreach (var word in words)
+        {
+            System.Console.WriteLine($"<{word}>");
+        }
+
+        string[] values = {"gloob","90"};
+        double number;
+        /* Attempt to parse each value in the array; if it cannot be parsed, write an error message in the console. */
+        foreach (string value in values) {
+            try {
+                number = double.Parse(value);
+            }
+            catch (FormatException) {
+                Console.WriteLine("Unable to parse.");
+            }
+        }
+    }
+}
