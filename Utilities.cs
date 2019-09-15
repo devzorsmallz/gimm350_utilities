4d72a2410351758aff07095bb59930d0a076a054
devzorsmallz <46795817+devzorsmallz@users.noreply.github.com>
2019-09-15 17:51:03 -0600

    Updated Utilities.cs

--- Utilities.cs
+++ Utilities.cs
@@ -72,4 +72,18 @@ public static class Utilities
         avgLetters = numLetters / numWords;
         return avgLetters.ToString();
     }
+
+        // Returns a list of all Materials to go on this GameObject and its children
+        static public Material[] GetAllMaterials(GameObject go)
+        {
+            Renderer[] rends = go.GetComponentsInChildren<Renderer>();
+
+            List<Material> mats = new List<Material>();
+            foreach (Renderer rend in rends)
+            {
+                mats.Add(rend.material);
+            }
+
+            return (mats.ToArray());
+        }
 }
